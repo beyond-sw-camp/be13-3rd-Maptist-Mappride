cd3bd5def570b08d40068fec7085d309187fe0dd
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-03-27 12:22:02 +0900

    카테고리 검색

--- frontend/src/stores/pinia.js
+++ frontend/src/stores/pinia.js
@@ -23,22 +23,6 @@ export const usePiniaStore = defineStore('auth', () => {
     }
     };
 
-    const getMyCategories = async() => {
-        try {
-            const response = await apiClient.get('/categories');
-      
-            console.log(response.data);
-      
-            if (response.status === 200) {
-              categories.value = response.data;
-            } else {
-              console.error('API 요청 실패:', response.status);
-            }
-          } catch (error) {
-            console.error('API 요청 실패:', error);
-            alert('검색 실패. 다시 시도해주세요.');
-          }
-    }
 
     // newPlaceComponent.vue 에서 사용
     let latitude = '';
@@ -55,6 +39,30 @@ export const usePiniaStore = defineStore('auth', () => {
 
     const getCategoryId = (categoryIdData) => {
         categoryId = categoryIdData;
+    };
+
+    // MyCategoryComponent.vue 에서 사용
+    const myCategories = ref([]);
+
+    const getMyCategories = async (categoryName) => {
+    try{
+        alert('들어오나용');
+        const response = await apiClient.get(`/categories/${categoryName}`);
+
+        if (response.status === 200) {
+            myCategories.value = response.data;
+
+            alert(myCategories.value);
+            alert(response.data);
+
+        } else {
+            console.error('API 요청 실패:', response.status);
+        }
+    }catch(error)
+    {
+        console.error('API 요청 실패:', error);
+        alert('검색 실패. 다시 시도해주세요.');
+    }
     };
 
     return { categories, getCategory, latitude, getLatitude, longitude, getLongitude, categoryId, getCategoryId, getMyCategories};
